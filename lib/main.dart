1e46ed4ec603c2fb7fbe31ddb5c4bfdcdb4f6407
Tapas <tapassaha6685@gmail.com>
2021-12-07 00:34:36 +0530

    DAY 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/pages/login_page.dart';
 import 'package:flutter_app/pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       // initialRoute: "/home",
       routes: {
         "/": (context) => LoginPage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,52 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
+      color: Colors.white,
+      child: Column(children: [
+        Image.asset(
+          "assestes/images/undraw_secure_login_pdn4.png",
+          fit: BoxFit.cover,
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          "Welcome To My Secure Login",
           style: TextStyle(
             fontSize: 20,
-            color: Colors.blueAccent,
             fontWeight: FontWeight.bold,
           ),
         ),
-      ),
+        SizedBox(
+          height: 40,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+          child: Column(children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Enter username",
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Enter password",
+                labelText: "Password",
+              ),
+            )
+          ]),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              print("Thank you for Login!!");
+            },
+            // style: ButtonStyle(
+            //   foregroundColor:
+            // ),
+            child: Text("Login"))
+      ]),
     );
   }
 }
